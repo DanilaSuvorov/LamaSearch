16abcc4e3e2501e459f54c1255547ecae3977234
DanilaSuvorov <dansuvtim@gmail.com>
2019-11-10 16:13:46 +0300

    start_of_searching

--- js/apps.js
+++ js/apps.js
@@ -16,11 +16,30 @@ firebase.analytics();
 var count = 5;
 let arr = [];
 
-for(var i = 1; i <= count; i++) {
-    var ref = firebase.database().ref().child("language").child("name" + i);
-    ref.on("value", function (snapshot) {
-        arr.push(snapshot.val());
-    });
+if(document.getElementById("btn").onclick === true){
+    var answer = document.getElementById("input").value;
 }
 
+if(arr.indexOf(answer) !== -1){
+    for(var i = 1; i <= count; i++) {
+        var ref = firebase.database().ref().child("Framework").child('NET').child(answer).child('idea' + i);
+        ref.on("value", function (snapshot) {
+            console.log(snapshot.val());
+        });
+    }
+}
+
+
 console.log(arr);
+
+var tagToQuery = answer;
+ref.once('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+        var childData = childSnapshot.val();
+        console.log(childData.name)
+        // ...
+    });
+});
+
+
+
